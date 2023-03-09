30fcc7df41f6228b778fa4e448bee871c05b07e9
TAKATSU MARINA <75340313+marina-t1123@users.noreply.github.com>
2023-03-10 06:53:06 +0900

    Bootstrapを使用するため、headタグの変更をしている。

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- viteではなく、Laravel-mixを使用する為コメントアウトしている。 -->
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+        <!-- Laravel-mixを使用する際は、下記２つを追加で読み込む必要がある -->
+        <link rel="stylesheet" href="{{ mix('css/app.css')}}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
