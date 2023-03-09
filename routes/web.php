9b75daff5de30cd41f117e3d3e965fc392abc7ad
TAKATSU MARINA <75340313+marina-t1123@users.noreply.github.com>
2023-03-10 06:51:24 +0900

    TOPページのルーティング追加。

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+// TOPページ
+Route::get('/lobook', function () {
+    return view('top');
+});
+
 require __DIR__.'/auth.php';
