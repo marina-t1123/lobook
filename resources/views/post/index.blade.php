0a5f5c4bae0e0a29d387a04d446999252903f8d9
TAKATSU MARINA <75340313+marina-t1123@users.noreply.github.com>
2023-03-16 14:10:33 +0900

    TOP画面のブレード

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Lobook</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Hina+Mincho&display=swap" rel="stylesheet">
+
+        <!-- style -->
+        <link rel="stylesheet" href="{{ mix('css/app.css')}}">
+
+        <!-- script -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body class="body-bg">
+        <!-- header -->
+        <header class="sticky-top" >
+            <!-- ナビ -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Lobook</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!-- ナビゲーション -->
+                    <div class="collapse navbar-collapse show" id="navbarNavAltMarkup">
+                        <!-- ナビメニュー -->
+                        <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link " href="{{ route('register')}}">Sing up</a>
+                        <a class="nav-link disabled" href="{{ route('login')}}" tabindex="-1" aria-disabled="true">Log in</a>
+                        {{-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> --}}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- カルーセル -->
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/images/top.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/top2.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!-- メイン -->
+        <main>
+            <div class="h1">Lobook</div>
+            <p>
+                このサービスは、お気に入りの本を紹介していく掲示板です。
+            </p>
+
+            <!-- 一覧 -->
+
+
+        </main>
+        <!-- footer -->
+        <footer></footer>
+    </body>
+</html>
